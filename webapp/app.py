0f65ff400402e7b3ddc1b43849858d98df92a76c
david-che <99378689+david-che@users.noreply.github.com>
2023-05-31 23:24:31 +0300

    Update app.py

--- webapp/app.py
+++ webapp/app.py
@@ -10,6 +10,6 @@ def hello():
     return 'Hello '+provider+'!'
 
 if __name__ == '__main__':
-    # Bind to PORT if defined, otherwise default to 5000.
-    port = int(os.environ.get('PORT', 5000))
+    # Bind to PORT if defined, otherwise default to 5001.
+    port = int(os.environ.get('PORT', 5001))
     app.run(host='0.0.0.0', port=port)
